e73e34d2ff2eb1353e98aeb32228097419fc4023
Beerscheba C <designergo@aol.com>
2018-07-01 17:33:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function placeOrder(cardNumber) {
 if (cardNumber) {  
   for (var i = cart.length; i >= 0; i--) 
 			cart.pop();
-return `Your total cost is $${sum}, which will be charged to the card ${cardNumber}.` }
+return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.` }
 			
 	  else  { 
 	  return 'Sorry, we don\'t have a credit card on file for you.' }
